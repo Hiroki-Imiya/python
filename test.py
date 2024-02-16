e770db16dd9f550b87cc7833e5853ef329f7f4ba
Hiroki-Imiya <yo4ro6si4ku9na7@gmail.com>
2024-02-16 15:52:26 +0900

    paizaラーニングタプルをアンパックまで実行

---
+++ test.py
@@ -0,0 +1,15 @@
+print("Hello World!", end="\n")
+t="paiza","learning"
+print(t)
+list_taple=["paiza","learning"],[1,2,3]
+print(list_taple)
+e=99,"drinks"
+print(e)
+s = "paiza"
+li = [1, 2, 3, 4, 5]
+a=(s,li)
+print(a)
+b,c,d=1,2,3
+print(b)
+print(c)
+print(d)
